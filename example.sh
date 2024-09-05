b14bb1374d4800308cea424a0a4d9598745983d6
alexey.zh <mailbox.stdio@gmail.com>
2024-09-05 20:34:28 +0500

    pretty print usage

--- example.sh
+++ example.sh
@@ -2,19 +2,20 @@
 set -euo pipefail
 
 usage() {
-  cat <<EOF
-Usage: $(basename "$0") [OPTION]
+	cat <<EOF
+Usage: $(basename "$0") [OPTIONS]
 
-Options:
-  --help
-  -d, --dbname           database to dump
-  -h, --host             database server host or socket directory
-  -p, --port             database server port number
-  -U, --username         connect as specified database user
-  -O, --output           output path
-  -n, --schema           dump only schemas matching pattern
-  -N, --exclude-schema   do not dump any schemas matching pattern
-  -v, --verbose
+OPTION                DESCRIPTION                             
+--help                                                        
+-d, --dbname          database to dump                        
+-h, --host            database server host or socket directory
+-p, --port            database server port number             
+-U, --username        connect as specified database user      
+-O, --output          output path                             
+-n, --schema          dump only schemas matching pattern      
+-N, --exclude-schema  do not dump any schemas matching pattern
+-v, --verbose                                                 
+
 EOF
 }
 
@@ -30,17 +31,17 @@ main() {
 
   VALID_ARGS=$(getopt -o d:h:p:U:O:n:N:v --long dbname:,host:,port:,username:,output:,schema:,exclude-schema:,verbose,help -- "$@")
 
-  # shellcheck disable=SC2181
-  if [ $? != 0 ]; then
-    printf "error parsing options"
-    usage
-    exit 1
-  fi
+	# shellcheck disable=SC2181
+	if [ $? != 0 ]; then
+		printf "error parsing options"
+		usage
+		exit 1
+	fi
 
-  eval set -- "$VALID_ARGS"
-  while true; do
-    case "$1" in
-
+	eval set -- "$VALID_ARGS"
+	while true; do
+		case "$1" in
+		
     -d | --dbname)
       dbname="${2}"
       shift 2
@@ -78,27 +79,27 @@ main() {
       exit 0
       ;;
 
-    --)
-      shift
-      break
-      ;;
-    *)
-      echo "unexpected argument ${1}"
-      usage
-      exit 1
-      ;;
-    esac
-  done
+		--)
+			shift
+			break
+			;;
+		*)
+			echo "unexpected argument ${1}"
+			usage
+			exit 1
+			;;
+		esac
+	done
 
-  # check remaining
-  shift $((OPTIND - 1))
-  remaining_args="${*}"
-  if [ -n "${remaining_args}" ]; then
-    echo "remaining args are not allowed: ${remaining_args[*]}"
-    usage
-    exit 1
-  fi
-
+	# check remaining
+	shift $((OPTIND - 1))
+	remaining_args="${*}"
+	if [ -n "${remaining_args}" ]; then
+		echo "remaining args are not allowed: ${remaining_args[*]}"
+		usage
+		exit 1
+	fi
+		
   # set checks
   if [ -z "${dbname}" ]; then
     printf "\n[error] required arg is empty: dbname\n\n"
@@ -135,6 +136,9 @@ main() {
   echo "schema=${schema[*]}"
   echo "exclude_schema=${exclude_schema[*]}"
   echo "verbose=${verbose}"
+
 }
 
 main "${@}"
+
+
